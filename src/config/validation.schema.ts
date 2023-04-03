074b15607dcbdce9fe8a8e1965cd0423cdad1b3d
ford10082 <ford10082@nsmg21.com>
2023-04-03 17:14:08 +0900

    Chore: add Joi validation

--- src/config/validation.schema.ts
+++ src/config/validation.schema.ts
@@ -21,10 +21,22 @@ export const validationSchema = Joi.object({
   // JWT
   JWT_SECRET: Joi.string().required(),
 
-  // Admin
+  // Swagger
   ADMIN_USERNAME: Joi.string().required().min(5),
   ADMIN_PASSWORD: Joi.string().required().min(10),
 
-  // Swagger
   SWAGGER_URL: Joi.string().required(),
+
+  // ETC
+  FRONT_LOGIN_URL: Joi.string().required().uri(),
+
+  // Azure
+  // AZURE_CDN_URL: 
+  AZURE_STORAGE_CONTAINER: Joi.string().required(),
+  AZURE_STORAGE_URL: Joi.string().required().uri(),
+  AZURE_CONNECTION_STRING: Joi.string().required(),
+
+  // Redis
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.number().required(),
 });
